efa4abf7a503e78170b0c6c25a31cae34d2287cc
Edward Delaporte <edthedev@gmail.com>
2016-10-04 21:40:42 -0500

    change lanes works.

--- static/road.js
+++ static/road.js
@@ -2,52 +2,29 @@
 var YourCar = {
 	lane: 0,
 	left: function() {
-		lane -= 1;
-		animate_to(lane);
+		YourCar.lane -= 1;
+		YourCar.animate_to();
 	},	
 	right: function() {
-		lane += 1;
-		animate_to(lane);
+		YourCar.lane += 1;
+		YourCar.animate_to();
 	},
 	animate_to: function() {
-		if(lane < 1) lane = 1;
-		if(lane > 3) lane = 3;
+		if(YourCar.lane < 1) YourCar.lane = 1;
+		if(YourCar.lane > 3) YourCar.lane = 3;
 		// var topPos = $('#' + item_id).offset()['top'] + 3;
-		var leftPos = lane_id * 200;
+		var leftPos = YourCar.lane * 200;
 		$("#car").animate({'left':leftPos+ 'px'}, "fast");
 	}
 };
 
-function khandle(e) {
-  e = e || event
-  if (document.forms.keyform[e.type + 'Ignore'].checked) return
-  if(e.keyCode == 37) YourCar.left();
-  if(e.keyCode == 39) YourCar.right();
-}
-document.getElementById('road').onkeydown = khandle
-// document.getElementById('road').onkeyup = khandle
-document.getElementById('road').onkeypress = khandle
-
-
-// 37 = left
-// 39 == right
-    
-/*
-  var evt = e.type
-  while (evt.length < 10) evt += ' '
-  showmesg(evt +
-    ' keyCode=' + e.keyCode +
-    ' which=' + e.which +
-    ' charCode=' + e.charCode +
-    ' char=' + String.fromCharCode(e.keyCode || e.charCode) +
-    (e.shiftKey ? ' +shift' : '') +
-    (e.ctrlKey ? ' +ctrl' : '') +
-    (e.altKey ? ' +alt' : '') +
-    (e.metaKey ? ' +meta' : ''), 'key'
-  )
-   
-  if (document.forms.keyform[e.type + 'Stop'].checked) {
-    e.preventDefault ? e.preventDefault() : (e.returnValue = false)
-  }
-*/
-
+$(document).ready( function() {
+	// document.getElementById('road').onkeydown = khandle
+	// document.getElementById('road').onkeyup = khandle
+	// document.getElementById('road').onkeypress = khandle
+	document.body.addEventListener('keydown', function (e) {
+		  e = e || event;
+		  if(e.keyCode == 37) YourCar.left();
+		  if(e.keyCode == 39) YourCar.right();
+	});
+});
